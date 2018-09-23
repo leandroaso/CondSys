ce5948c0189ce60c8581427c6e839006fcb331bb
Leandro Alves <leandro.aso123@outlook.com>
2018-09-22 21:13:40 -0300

    separando o javascript da view

--- CondSys/wwwroot/js/site.js
+++ CondSys/wwwroot/js/site.js
@@ -1,4 +1,102 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$('.datetimepicker').mask('99/99/9999', { placeholder: "mm/dd/yyyy" });
 
-// Write your JavaScript code.
+function Edit(data) {
+    limparFormulario()
+    var modal = $("#myModal");
+    modal.find("#Assembleia_Id").val(data.Id);
+    modal.find("#Assembleia_Data").val(data.Data);
+    modal.find("#Assembleia_Titulo").val(data.Titulo);
+    modal.find("#Assembleia_Descricao").html(data.Descricao);
+
+    modal.modal('show');
+}
+
+function Cacelar(id, url) {
+    const swalWithBootstrapButtons = swal.mixin({
+        confirmButtonClass: 'btn btn-success',
+        cancelButtonClass: 'btn btn-danger',
+        buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.value) {
+            console.log(id);
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: "id=" + id,
+                success: function (response) {
+                    if (response == "success") {
+                        location.reload();
+                    } else {
+                        swal(
+                            'Ops!',
+                            'ouvi um problema ao tentar cencelar a assembleia, tente novamente mais tarde!',
+                            'warning'
+                        ).then(() => {
+                            location.reload();
+                        });
+                    }
+                },
+                error: function (response) {
+                    swal(
+                        'Erro!',
+                        'ouvi um problema ao tentar cencelar a assembleia, tente novamente mais tarde!',
+                        'error'
+                    ).then(() => {
+                        location.reload();
+                    });
+                }
+
+            });
+        }
+    });
+}
+
+function formSubmit() {
+
+    var form = $("#fomulario");
+
+    $.ajax({
+        type: "POST",
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: function (response) {
+            if (response == "success") {
+                location.reload();
+            } else {
+                swal(
+                    'Ops!',
+                    'ouvi um problema ao tentar realizar o cadastro, verifique se os dados estão corretos',
+                    'warning'
+                ).then(() => {
+                    location.reload();
+                });
+            }
+        },
+        error: function (response) {
+            swal(
+                'Erro!',
+                'ouvi um problema ao tentar realizar o cadastro, verifique se os dados estão corretos',
+                'error'
+            ).then(() => {
+                location.reload();
+            });
+        }
+
+    });
+}
+
+function limparFormulario() {
+    $('#fomulario input').val("");
+    $('#fomulario textarea').html("");
+}
